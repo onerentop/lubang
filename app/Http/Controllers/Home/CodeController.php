af453fc403f144dd9963a2e737b081aa31a794ee
wangren <819699622@qq.com>
2017-05-09 13:13:16 +0800

    init

--- app/Http/Controllers/Home/CodeController.php
+++ app/Http/Controllers/Home/CodeController.php
@@ -58,6 +58,8 @@ class CodeController extends Controller
     {
         $tel = Input::get('tel');
         $num = mt_rand(100000, 999999);
+        DB:
+        table('verify')->truncate();
         DB::table('verify')->insert(['tel' => $tel, 'code' => $num]);
         $this->duan($tel, $num);
     }
