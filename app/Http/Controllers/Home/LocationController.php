20c8fe487e5c32ef95ff80268cc7cf1c45f712d7
wangren <819699622@qq.com>
2017-05-09 16:04:38 +0800

    init

--- app/Http/Controllers/Home/LocationController.php
+++ app/Http/Controllers/Home/LocationController.php
@@ -65,11 +65,11 @@ class LocationController extends Controller
         $arr = [];
         $lg1 = $lg + 1;
         $lt1 = $lt + 1;
-        $result = DB::table('location')->where('longitude', '>', $lg)->whereBetween('longitude', [$lg, $lg1])->whereBetween('latitude', [$lt, $lt1])->get();
+        $result = DB::table('location')->whereBetween('longitude', [$lg, $lg1])->whereBetween('latitude', [$lt, $lt1])->get();
         foreach ($result as $value) {
             $arr[] = $value;
         }
-        dd($lg);
+        dd($result);
         return jsondata(1, '位置信息', $arr);
     }
 
