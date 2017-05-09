d28b445e4976b674fe118c9a4d8d2bd73d82bc26
wangren <819699622@qq.com>
2017-05-09 17:11:09 +0800

    init

--- app/Http/Controllers/Home/LocationController.php
+++ app/Http/Controllers/Home/LocationController.php
@@ -114,8 +114,13 @@ class LocationController extends Controller
             foreach ($res as $value) {
                 $rescue_id = $value->id;
                 $status = 1;
-                DB::table("service_request")->where(['buyer_id' => $user_id, 'seller_id' => $rescue_id])->delete();
-                $result = DB::table('service_request')->insert(['buyer_id' => $user_id, 'seller_id' => $rescue_id, 'money' => $money, 'time' => $time, 'location_id' => $location_id, 'status' => $status]);
+                $service = DB::table("service_request")->where(['buyer_id' => $user_id, 'seller_id' => $rescue_id])->get();
+                if (!$service) {
+                    $result = DB::table('service_request')->insert(['buyer_id' => $user_id, 'seller_id' => $rescue_id, 'money' => $money, 'time' => $time, 'location_id' => $location_id, 'status' => $status]);
+                } else {
+                    $result = DB::table('service_request')->where(['buyer_id' => $user_id, 'seller_id' => $rescue_id])->data(['money' => $money, 'time' => $time, 'location_id' => $location_id, 'status' => $status])->update();
+                }
+
                 if ($result) {
                     echo jsondata(1, '存储成功', []);
                 } else {
