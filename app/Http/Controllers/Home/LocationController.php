fe2220d8a5ef35c973fdf31107fdf0718d90153b
wangren <819699622@qq.com>
2017-05-09 14:58:38 +0800

    init

--- app/Http/Controllers/Home/LocationController.php
+++ app/Http/Controllers/Home/LocationController.php
@@ -28,7 +28,7 @@ class LocationController extends Controller
      */
     public function saveLocation($lg, $lt, $tel, $address_info)
     {
-        $user_id = DB::table('users')->select('id')->where(['tel' => $tel])->get()->toArray();
+        $user_id = DB::table('users')->select('id')->where(['tel' => $tel])->get()->toArray()[0];
         dd($user_id);
         die();
         DB::table('location')->where('user_id', '=', $user_id)->delete();
