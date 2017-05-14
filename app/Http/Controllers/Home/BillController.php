67ef01a4907f197fb207b418851ac1c51051d6c9
wangren <819699622@qq.com>
2017-05-15 00:21:09 +0800

    init

--- app/Http/Controllers/Home/BillController.php
+++ app/Http/Controllers/Home/BillController.php
@@ -20,7 +20,12 @@ class BillController extends Controller
         $re = DB::table('service_request')->where(['id' => $service_request_id])->get()->toArray();
         dd($re);
         $time = date("Y-m-d H:i:s");
-//        DB::table()->insert(['buyer_id'=> ,'seller_id'=> ,'money'=> ,'time'=>$time ,'status'=>1]);
+        $res = DB::table()->insert(['buyer_id' => $re[0]->buyer_id, 'seller_id' => $re[0]->seller_id, 'money' => $re[0]->money, 'time' => $time, 'status' => 1]);
+        if ($res) {
+            return jsondata(1, 'success', []);
+        } else {
+            return jsondata(0, 'faile', []);
+        }
     }
 
     //查询用户账单
