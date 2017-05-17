71b5d2971e41c1fa15976ee7bd26e3d59b97fa69
wangren <819699622@qq.com>
2017-05-17 22:26:22 +0800

    init

--- app/Http/Controllers/Home/LocationController.php
+++ app/Http/Controllers/Home/LocationController.php
@@ -37,9 +37,9 @@ class LocationController extends Controller
             $result = DB::table('location')->insert(['user_id' => $user_id, 'longitude' => $lg, 'latitude' => $lt, 'address_info' => $address_info]);
         }
         if ($result) {
-            echo jsondata(1, '存储成功', []);
+            return jsondata(1, '存储成功', []);
         } else {
-            echo jsondata(0, '存储失败', []);
+            return jsondata(0, '存储失败', []);
         }
     }
 
